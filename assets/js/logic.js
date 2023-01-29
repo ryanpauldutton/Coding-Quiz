5ec96dd19e5137d9811fd6194bed2bcab56f2dbf
rizla <ryanpauldutton@gmail.com>
2023-01-29 15:06:18 +0000

    made the buttons appear when press start game and title

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,18 +1,62 @@
+
+
+console.log(quizQuestions[0]['question']);
+
 let startButton = document.querySelector('#start');
 let timerElement = document.querySelector('#time');
 let startScreenEl = document.querySelector('#start-screen');
 let questionsScreenEl = document.querySelector('#questions');
+let choicesEl = document.querySelector('#choices')
+let questionTitleEl = document.querySelector('#question-title')
 
+// let questions1 = [big, small, medium, large];
+// let questions2 = [big, small, medium, large];
+// let questions3 = [big, small, medium, large];
+// let questions4 = [big, small, medium, large];
 
 let timer;
 let timerCount;
 
 function startGame() {
-    timerCount = 10;
+    timerCount = 50;
     // Prevents start button from being clicked when round is in progress
     startScreenEl.classList.add('hide')
     questionsScreenEl.classList.remove('hide')
     startTimer();
+
+    questionTitleEl.textContent = quizQuestions[0]['question'];
+
+    // var choiceOne = document.createElement("button");
+    // choicesEl.append(choiceOne);
+    // choiceOne.textContent = quizQuestions[0]['choices'][0];
+
+    // var choiceTwo = document.createElement("button");
+
+    // choicesEl.append(choiceTwo);
+    // choiceTwo.textContent = quizQuestions[0]['choices'][1];
+
+    // var choiceThree = document.createElement("button");
+
+    // choicesEl.append(choiceThree);
+    // choiceThree.textContent = quizQuestions[0]['choices'][2];
+
+    // var choiceFour = document.createElement("button");
+
+    
+    //   choicesEl.append(choiceFour);
+    //   choiceFour.textContent = quizQuestions[0]['choices'][3];
+
+      for(var x = 0; x < 4; x++){
+        var button = document.createElement('button');
+        button.textContent = quizQuestions[0]['choices'][x];
+        choicesEl.appendChild(button);
+      }
+
+    // var addButton1 = document.createElement("button");
+    // choicesEl.append(addButton1);
+    // addButton1.textContent = quizQuestions[0]['choices'][0];
+
+
 
   }
 
@@ -40,6 +84,10 @@ function startGame() {
   
 
   startButton.addEventListener("click", startGame);
+
+
+
+
 
 // Button to CHANGE PAGE LOCATION
 
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,4 +1,53 @@
+let quizQuestions = [
+    {
+        question: 'how big is the window?',
+        choices: [
+            'big',
+            'small',
+            'medium',
+            'large'
+        ],
+        correctAnswerIndex: 3
+    },
+
+    {
+        question: 'how big is the window?',
+        choices: [
+            'big',
+            'small',
+            'medium',
+            'large'
+        ],
+        correctAnswerIndex: 2
+    },
+
+    {
+        question: 'how big is the window?',
+        choices: [
+            'big',
+            'small',
+            'medium',
+            'large'
+        ],
+        correctAnswerIndex: 1
+    },
+
+    {
+        question: 'how big is the window?',
+        choices: [
+            'big',
+            'small',
+            'medium',
+            'large'
+        ],
+        correctAnswerIndex: 2
+    },
+]
+
+// let choicesEl = document.querySelector('#choices')
+
+// choicesEl.addElement('button').innerText = Hi der
+
 // four option buttons
 
 //change inner text
-
